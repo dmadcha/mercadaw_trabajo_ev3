ad0603d9e43305bec7f0ca4dd6591da6502b6b32
DiegoMadCha <diego@mail.com>
2024-04-16 12:55:45 +0200

    initial commit

---
+++ src/main/java/es/etg/daw/prog/mercadaw/PrimaryController.java
@@ -0,0 +1,12 @@
+package es.etg.daw.prog.mercadaw;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
