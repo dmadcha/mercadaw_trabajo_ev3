510bcde48a178b1d23bd09b8f395be3d27b157ca
jesus <jesus@mail.com>
2024-05-16 11:55:53 +0200

    Mover la BBDDException al paquete exception

--- src/main/java/es/etg/daw/prog/mercadaw/BBDDException.java
+++
@@ -1,7 +0,0 @@
-package es.etg.daw.prog.mercadaw;
-
-import es.etg.daw.prog.mercadaw.model.exception.MercaDAWException;
-
-public class BBDDException extends MercaDAWException {
-    
-}
---
+++ src/main/java/es/etg/daw/prog/mercadaw/model/exception/BBDDException.java
@@ -0,0 +1,5 @@
+package es.etg.daw.prog.mercadaw.model.exception;
+
+public class BBDDException extends MercaDAWException {
+    
+}
