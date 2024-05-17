0f2dadc2155631186a637727e8b2fe4222e17861
dmadcha <dmadcha@mail.com>
2024-05-17 19:24:03 +0200

    PruebaCrearProducto

--- src/test/java/es/etg/daw/prog/mercadaw/model/entities/ProductoTest.java
+++ src/test/java/es/etg/daw/prog/mercadaw/model/entities/ProductoTest.java
@@ -3,16 +3,15 @@ package es.etg.daw.prog.mercadaw.model.entities;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
-import es.etg.daw.prog.mercadaw.model.entities.productos.Drogueria;
 import es.etg.daw.prog.mercadaw.model.entities.productos.Producto;
+import es.etg.daw.prog.mercadaw.model.entities.productos.Drogueria;
 
 public class ProductoTest {
     @Test
     public void crearProducto(){
-        Producto pr = new Drogueria();
+        Producto pr = new Drogueria("Cafetera Express", "BaristaPro", 249.99, 35, 20, 4.5, 1, "La Cafetera Express Automática de BaristaPro ofrece una experiencia de café de calidad profesional en la comodidad de tu hogar. Con una bomba de presión de 15 bares y un sistema de calentamiento rápido, esta cafetera prepara espresso, cappuccino y latte con un sabor y aroma inigualables. Su diseño compacto y elegante se adapta a cualquier cocina");
         assertEquals(1, Producto.getNumProductos());
-        Producto pr2 = new Drogueria();
+        Producto pr2 = new Drogueria("Juego de Ollas de Cocina", "ChefMaster", 189.99, 45, 30, 6.2, 10, "El Juego de Ollas de Cocina ChefMaster incluye 10 piezas esenciales para cualquier cocina. Fabricadas con acero inoxidable de alta calidad y fondo de aluminio encapsulado para una distribución uniforme del calor, estas ollas y sartenes son ideales para cocinar todo tipo de recetas. El set incluye 2 cacerolas con tapa, 2 ollas con tapa, 1 sartén grande y 1 sartén pequeña, perfectas para cocineros principiantes y expertos. Apta para lavavajillas y compatible con todas las superficies de cocina, incluyendo inducción.");
         assertEquals(2, Producto.getNumProductos());
     }
 }
