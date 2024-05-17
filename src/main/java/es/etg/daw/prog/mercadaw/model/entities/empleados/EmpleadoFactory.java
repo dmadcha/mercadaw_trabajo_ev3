11ab137ae2fc19754344f9933694d9f5969b228b
dmadcha <dmadcha@mail.com>
2024-05-17 20:05:06 +0200

    Entities changes

--- src/main/java/es/etg/daw/prog/mercadaw/model/entities/empleados/EmpleadoFactory.java
+++ src/main/java/es/etg/daw/prog/mercadaw/model/entities/empleados/EmpleadoFactory.java
@@ -3,14 +3,22 @@ package es.etg.daw.prog.mercadaw.model.entities.empleados;
 import es.etg.daw.prog.mercadaw.model.exception.MercaDAWException;
 
 public class EmpleadoFactory {
-    public static Empleado obtener(TipoEmpleado tipo, String nombre, String apellidos) throws MercaDAWException{
-        if (tipo.equals(TipoEmpleado.REPONEDOR)) {
+    public static Empleado obtener(String tipoString, String nombre, String apellidos) throws MercaDAWException{
+        
+        TipoEmpleado tipoEmpleado;
+        try {
+            tipoEmpleado = TipoEmpleado.valueOf(tipoString.toUpperCase());
+        } catch (Exception e) {
+            throw new MercaDAWException();
+        }
+
+        if (tipoEmpleado.equals(TipoEmpleado.REPONEDOR)) {
             return new Reponedor(nombre, apellidos);
-        } else if (tipo.equals(TipoEmpleado.CAJERO)) {
+        } else if (tipoEmpleado.equals(TipoEmpleado.CAJERO)) {
             return new Cajero(nombre, apellidos);
-        } else if (tipo.equals(TipoEmpleado.ENCARGADO)) {
+        } else if (tipoEmpleado.equals(TipoEmpleado.ENCARGADO)) {
             return new Encargado(nombre, apellidos);
-        } else if (tipo.equals(TipoEmpleado.EMPLEADO)) {
+        } else if (tipoEmpleado.equals(TipoEmpleado.EMPLEADO)) {
             return new Empleado(nombre, apellidos);
         } else
         throw new MercaDAWException();
--- src/main/java/es/etg/daw/prog/mercadaw/model/entities/productos/Alimentacion.java
+++ src/main/java/es/etg/daw/prog/mercadaw/model/entities/productos/Alimentacion.java
@@ -2,9 +2,9 @@ package es.etg.daw.prog.mercadaw.model.entities.productos;
 
 public class Alimentacion extends Producto{
 
-    public Alimentacion(String nombre, String marca, double precio, double altura, double anchura, double peso,
+    public Alimentacion(String nombre, String marca, double altura, double anchura, double peso,
             int numElementos, String descripcion) {
-        super(nombre, marca, precio, altura, anchura, peso, numElementos, descripcion);
+        super(nombre, marca, altura, anchura, peso, numElementos, descripcion);
         this.iva = IVA_ALIM;
     }
 
