c6d1a93907feac992c08a4c6f3ebbca4ad7e645a
Tu nombre <iv@gmail.com>
2024-05-18 13:19:44 +0200

    Copias de seguridad

--- src/main/resources/script/Copia_Seguridad.sh
+++ src/main/resources/script/Copia_Seguridad.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-RUTA_ARCHIVO="archivo/export.txt"
-RUTA_COLOCAR="backups"
 FECHA=$(date +"%Y_%m_%d_%H")
 BACKUP="copiaoficial_$FECHA.txt" 
 
-cp $RUTA_ARCHIVO  $RUTA_COLOCAR/$BACKUP
+cp "$1" "$2/$BACKUP"
+
 
 if [ $? -eq 0 ]; then
-        echo "Copia de seguridad creada ${BACKUP}"
+    echo "Copia de seguridad creada: ${RUTA_COLOCAR}/${BACKUP}"
 else
-        echo "Error al realizar copia de seguridad"
-        exit 1
+    echo "Error al realizar copia de seguridad"
+    exit 1
 fi
-
-
