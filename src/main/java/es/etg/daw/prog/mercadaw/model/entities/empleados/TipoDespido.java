a6f2ce166c46dd1ad8011f360e3ba242015ba98d
DiegoMadCha <diego@mail.com>
2024-05-19 18:49:19 +0200

    EnumEmpleadosDoc

--- src/main/java/es/etg/daw/prog/mercadaw/model/entities/empleados/TipoDespido.java
+++ src/main/java/es/etg/daw/prog/mercadaw/model/entities/empleados/TipoDespido.java
@@ -1,5 +1,9 @@
 package es.etg.daw.prog.mercadaw.model.entities.empleados;
 
+/**
+ * La enumeracion TipoDespido representa los tipos de despidos posibles.
+ * \author Diego Madroñero Chamorro.
+ */
 public enum TipoDespido {
     PRODECENTE, IMPROCEDENTE
 }
--- src/main/java/es/etg/daw/prog/mercadaw/model/entities/empleados/TipoEmpleado.java
+++ src/main/java/es/etg/daw/prog/mercadaw/model/entities/empleados/TipoEmpleado.java
@@ -1,5 +1,9 @@
 package es.etg.daw.prog.mercadaw.model.entities.empleados;
 
+/**
+ * La enumeracion TipoEmpleado representa los tipos de empleados de MercaDAW.
+ * \author Diego Madroñero Chamorro.
+ */
 public enum TipoEmpleado {
     EMPLEADO, REPONEDOR, CAJERO, ENCARGADO
 }
